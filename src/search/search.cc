7fde87d0189a24b1c5b26eb21430ae2c520a9f79
seolhokim <kilmya1@naver.com>
2023-08-21 07:02:27 +0000

    feat first state sending

--- src/search/search.cc
+++ src/search/search.cc
@@ -89,7 +89,7 @@ void Search::run(int client, const Cfg& target, CostFunction& fxn, Init init, Se
     state.best_correct_cost = 0;
     return;
   }
-
+  fxn(client, state.current, 100000000);
   TransformInfo ti;
 
   give_up_now = false;
