817f88357313101d308a0b7803244984e2be42dc
seolhokim <kilmya1@naver.com>
2023-08-28 08:41:24 +0000

    refactor how to reset

--- tools/apps/stoke_search.cc
+++ tools/apps/stoke_search.cc
@@ -384,7 +384,7 @@ vector<string>& split(string& s, const string& delim, vector<string>& result) {
 int main(int argc, char** argv) {
   int client;
   int portnum = std::stoi(argv[6]);
-
+  
   argc -= 2; 
   const char* ip = "127.0.0.1";
 
@@ -595,8 +595,9 @@ int main(int argc, char** argv) {
       //reset end
 
     } catch (const std::runtime_error& e){
-      if (std::string(e.what()) == "restart"){
+      if (std::string(e.what()) == "restart-deprecated"){
         std::cout << "environment reset" << endl;
+        break
       }
     }
 
